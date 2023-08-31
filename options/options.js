4a504d251637df561f24c6c2bfcf4ef026c0313d
Crowdbotics <engineering@crowdbotics.com>
2023-08-31 12:37:13 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "billowing-tree-dev-113228",
+  url: "https://billowing-tree-dev-113228.botics.co",
+  api: "https://billowing-tree-dev-113228.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
